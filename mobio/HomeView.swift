99ca85d78cb208e00cbc927f9295a673aa57f9c5
devioss007 <87515341+devioss007@users.noreply.github.com>
2023-06-20 14:57:34 +0500

    Card view is added

--- mobio/HomeView.swift
+++ mobio/HomeView.swift
@@ -12,7 +12,7 @@ struct HomeView: View {
     
     @EnvironmentObject var profile: Profile
     let tokenService = UserDefaultsManager()
-
+    
     var body: some View {
         ZStack {
             Color.gray
@@ -31,7 +31,7 @@ struct HomeView: View {
                     VStack{
                         switch currentSection {
                         case .home:
-                            Text("Welcome to home")
+                            MainHomeView()
                         case .search:
                             Text("Welcome to Search")
                         case .basket:
@@ -101,3 +101,117 @@ struct Home_Previews: PreviewProvider {
     }
 }
 
+
+struct MainHomeView: View {
+    var body: some View {
+        
+        VStack {
+            HStack {
+                Text("Електроника")
+                    .font(.title3)
+                Spacer()
+                Text("Увидеть все")
+                    .foregroundColor(.red)
+                Image("Arrow")
+            }
+            .bold()
+            .padding()
+            ScrollView(.horizontal) {
+                HStack(spacing: 14) {
+                    ForEach(0..<10, id: \.self) { item in
+                        MainCardView()
+                            .padding(.bottom, 6)
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .scrollIndicators(.hidden)
+        }
+        .padding(.vertical)
+    }
+}
+
+struct MainCardView: View {
+    var body: some View {
+        VStack {
+            
+            VStack {
+                HStack(alignment: .top) {
+                    Text("11.76 %")
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                        .background(.pink)
+                        .cornerRadius(25)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 8) {
+                        
+                        Button {
+                            print("Heart button tapped")
+                        } label: {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 50)
+                                    .foregroundColor(.white)
+                                Image(systemName: "heart")
+                                    .font(.system(size: 24))
+                                    .foregroundColor(.gray)
+                            }
+                        }
+
+                        Button {
+                            print("Cart button tapped")
+                        } label: {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 50)
+                                    .foregroundColor(.white)
+                                Image(systemName: "cart")
+                                    .font(.system(size: 24))
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                    }
+                }
+                .padding(10)
+                Spacer()
+            }
+            .background(.gray.opacity(0.6))
+            
+            
+            VStack {
+                HStack {
+                    VStack {
+                        Text("150 000 сум")
+                            .bold()
+                        Text("170 000 сум")
+                            .strikethrough(true, color: .red)
+                            .foregroundColor(.secondary)
+                    }
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "star.fill")
+                        Text("3,00")
+                    }
+                    .foregroundColor(.white)
+                    .padding(7)
+                    .background(.pink)
+                    .cornerRadius(5)
+                }
+                .padding(8)
+                Text("Беспроводные наушники Airpods 2.2 lux copy")
+                    .padding(.bottom)
+            }
+            
+        }
+
+        .frame(width: 240, height: 300)
+        .background(.white)
+        .cornerRadius(10)
+        .shadow(radius: 3)
+    }
+}
