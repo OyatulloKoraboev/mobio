d1d46a4eac9f24750b8202efff01577909fdb551
devioss007 <87515341+devioss007@users.noreply.github.com>
2023-06-21 11:22:49 +0500

    Password show and hide button is added

--- mobio/View/Auth/Login.swift
+++ mobio/View/Auth/Login.swift
@@ -22,6 +22,7 @@ struct Login: View {
     @State var buttonState: Bool = false
     @State var showAlert: Bool = false
     @Binding var showProgressView: Bool
+    @State private var showPassword: Bool = false
     
     let service = NetworkManager()
     let tokenService = UserDefaultsManager()
@@ -29,80 +30,124 @@ struct Login: View {
     @State private var showingLoginScreen = false
     
     var body: some View {
-            VStack(alignment: .leading, spacing: 15) {
-                
-                VStack(alignment:.leading) {
-                    Text("Номер телефона")
-                        .font(.montserratRegular(size:10))
-                        .foregroundColor(Color(hex: "#AEAEAE"))
-                    MaskedTextField(
-                        text: $username,
-                        value: $phoneNumber,
-                        complete: $nice,
-                        placeholder: "+998",
-                        primaryMaskFormat: "+998 ([00]) [000]-[00]-[00]"
-                    )
-                    .frame(height: 40)
-                    .padding(.top, -8)
-                    .overlay(
-                        ZStack {
-                            GeometryReader { geometry in
-                                Rectangle()
-                                    .fill(Color(hex: "#AEAEAE"))
-                                    .frame(height: 3)
-                                    .frame(width: geometry.size.width)
-                                    .offset(y: 35)
-                                    .opacity(0.3)
-                            }
+        VStack(alignment: .leading, spacing: 15) {
+            
+            VStack(alignment:.leading) {
+                Text("Номер телефона")
+                    .font(.montserratRegular(size:10))
+                    .foregroundColor(Color(hex: "#AEAEAE"))
+                MaskedTextField(
+                    text: $username,
+                    value: $phoneNumber,
+                    complete: $nice,
+                    placeholder: "+998",
+                    primaryMaskFormat: "+998 ([00]) [000]-[00]-[00]"
+                )
+                .frame(height: 40)
+                .padding(.top, -8)
+                .overlay(
+                    ZStack {
+                        GeometryReader { geometry in
+                            Rectangle()
+                                .fill(Color(hex: "#AEAEAE"))
+                                .frame(height: 3)
+                                .frame(width: geometry.size.width)
+                                .offset(y: 35)
+                                .opacity(0.3)
                         }
-                    )
-                }
-                .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24))
+                    }
+                )
+            }
+            .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24))
+            
+            VStack(alignment:.leading) {
+                Text("Пароль")
+                    .font(.montserratRegular(size:10))
+                    .foregroundColor(Color(hex: "#AEAEAE"))
                 
-                
-                ExtractedView(text: $passwordText, title: "Пароль")
-                
-                Button {
-                    print("Востоновите Пароль")
-                } label: {
-                    Text("Забыли пароль ?")
-                        .font(.montserratRegular(size:14))
-                        .foregroundColor(.black)
-                }.padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 0))
-                
-                Spacer()
-                
-                Button {
-                    authenticateUser(username: phoneNumber, password: passwordText)
-                    
-                } label: {
-                    HStack {
-                        Spacer()
-                        Text("Авторизация")
-                            .font(.montserratRegular(size:20))
-                            .foregroundColor(.white)
-                        Spacer()
+                ZStack(alignment: .trailing) {
+                    if showPassword {
+                        TextField("", text: $passwordText)
+                            .frame(height: 40)
+                            .padding(.top, -8)
+                            .overlay(
+                                ZStack {
+                                    GeometryReader { geometry in
+                                        Rectangle()
+                                            .fill(Color(hex: "#AEAEAE"))
+                                            .frame(height: 3)
+                                            .frame(width: geometry.size.width)
+                                            .offset(y: 35)
+                                            .opacity(0.3)
+                                    }
+                                }
+                            )
+                    } else {
+                        SecureField("", text: $passwordText)
+                            .frame(height: 40)
+                            .padding(.top, -8)
+                            .overlay(
+                                ZStack {
+                                    GeometryReader { geometry in
+                                        Rectangle()
+                                            .fill(Color(hex: "#AEAEAE"))
+                                            .frame(height: 3)
+                                            .frame(width: geometry.size.width)
+                                            .offset(y: 35)
+                                            .opacity(0.3)
+                                    }
+                                }
+                            )
                     }
                     
+                    Button(action: {
+                        showPassword.toggle()
+                    }) {
+                        Image(systemName: showPassword ? "eye.fill" : "eye.slash.fill")
+                            .foregroundColor(.gray)
+                            .padding(.trailing, 10)
+                    }
                 }
-                
-                .frame(maxWidth: .infinity)
-                .frame(height: 55)
-                .background(
-                    LinearGradient(gradient: Gradient(colors: [Color(hex: "#E41B4D"), Color(hex: "#EE3D69")]), startPoint: .leading, endPoint: .trailing)
-                )
-                .opacity((phoneNumber.count == 9 && passwordText.count >= 8) ? 1.0 : 0.5)
-                .disabled(!(phoneNumber.count == 9 && passwordText.count >= 8))
-                .cornerRadius(25)
-                .padding(EdgeInsets(top: 0, leading: 30, bottom: 40, trailing: 30))
-                NavigationLink(destination: HomeView(), isActive: $loggedIn) {
-                    EmptyView()
-                }
-                
-                
-                
+            }
+            .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24))
             
-     
+            Button {
+                print("Востоновите Пароль")
+            } label: {
+                Text("Забыли пароль ?")
+                    .font(.montserratRegular(size:14))
+                    .foregroundColor(.black)
+            }.padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 0))
+            
+            Spacer()
+            
+            Button {
+                authenticateUser(username: phoneNumber, password: passwordText)
+                
+            } label: {
+                HStack {
+                    Spacer()
+                    Text("Авторизация")
+                        .font(.montserratRegular(size:20))
+                        .foregroundColor(.white)
+                    Spacer()
+                }
+                
+            }
+            
+            .frame(maxWidth: .infinity)
+            .frame(height: 55)
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color(hex: "#E41B4D"), Color(hex: "#EE3D69")]), startPoint: .leading, endPoint: .trailing)
+            )
+            .opacity((phoneNumber.count == 9 && passwordText.count >= 8) ? 1.0 : 0.5)
+            .disabled(!(phoneNumber.count == 9 && passwordText.count >= 8))
+            .cornerRadius(25)
+            .padding(EdgeInsets(top: 0, leading: 30, bottom: 40, trailing: 30))
+            NavigationLink(destination: HomeView(), isActive: $loggedIn) {
+                EmptyView()
+            }
+            
             
         }.navigationBarHidden(true)
             .customAlert(isPresented: $showAlert) {
@@ -149,9 +194,9 @@ struct Login: View {
         }
     }
     
-//    struct SignIn_Previews: PreviewProvider {
-//        static var previews: some View {
-//            Login()
-//        }
-//    }
+    //    struct SignIn_Previews: PreviewProvider {
+    //        static var previews: some View {
+    //            Login()
+    //        }
+    //    }
 }
